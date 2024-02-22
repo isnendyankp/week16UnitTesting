ca094caf84f5739bc1430dd6671920f75a962bbd
isnendyankp <isnendyankp@gmail.com>
2024-02-22 16:33:23 +0700

    cr8 dummy data array & test get list pokemons

--- UnitTesting/app/route/pokemon_route.py
+++ UnitTesting/app/route/pokemon_route.py
@@ -1,9 +1,36 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 
 # Create a blueprint object
 pokemon_blueprint = Blueprint('pokemon_endpoint', __name__)
 
+# data  array for the pokemons
+
+pokemons = [
+    {
+        "id": 1,
+        "name": "bulbasaur",
+        "type": "grass"
+    },
+    {
+        "id": 2,
+        "name": "charmander",
+        "type": "fire"
+    },
+]
+
 @pokemon_blueprint.route("/", methods=["GET"])
 def get_pokemon():
-    return 'Get Pokemon'
+    return jsonify(pokemons)
+
+@pokemon_blueprint.route("/", methods=["POST"])
+def create_pokemon():
+    return 'Create Pokemon'
+
+@pokemon_blueprint.route("/<int:pokemon_id>", methods=["GET"])
+def get_pokemon_by_id(pokemon_id):
+    return f'Get Pokemon by id {pokemon_id}'
+
+@pokemon_blueprint.route("/<int:pokemon_id>", methods=["PUT"])
+def update_pokemon(pokemon_id):
+    return f'Update Pokemon by id {pokemon_id}'
