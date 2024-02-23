326219b5c227dac4d6232871725a5a7af9b8fbeb
isnendyankp <isnendyankp@gmail.com>
2024-02-23 16:31:38 +0700

    Add database configuration to Flask app

--- UnitTesting/app/__init__.py
+++ UnitTesting/app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from app.route import pokemon_route
+import os
 
 app = Flask(__name__)
+
+# set the database URI
+DATABASE_TYPE = os.getenv("DATABASE_TYPE")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+app.config["SQLALCHEMY_DATABASE_URI"] = f"{DATABASE_TYPE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 # register the blueprints
 app.register_blueprint(pokemon_route.pokemon_blueprint, url_prefix="/pokemons")
