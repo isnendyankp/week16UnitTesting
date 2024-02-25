e0911626cd705729019ca8ccb98d92583473c2cd
isnendyankp <isnendyankp@gmail.com>
2024-02-25 20:06:01 +0700

    Add Customer model to app/models/customer.py

--- UnitTesting/app/models/customer.py
+++ UnitTesting/app/models/customer.py
@@ -1,2 +1,6 @@
 form utils.database import db
 
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(100), nullable=True)
