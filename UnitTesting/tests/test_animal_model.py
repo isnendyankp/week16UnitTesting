6253ef5c6a3e76d86f97f21d526fae040c5b3375
isnendyankp <isnendyankp@gmail.com>
2024-03-06 05:43:29 +0700

    Add test for Animal Model response status code 200

--- UnitTesting/tests/test_animal_model.py
+++ UnitTesting/tests/test_animal_model.py
@@ -8,3 +8,10 @@ def client():
     with app.test_client() as client:
         yield client
 
+# Test for Animal Model
+
+# This Animal Model test for response status code 200 means that the request was successful
+def test_get_animals(client):
+    with app.app_context():
+        response = client.get('/animal/')
+        assert response.status_code == 200
